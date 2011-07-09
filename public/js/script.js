aa99aa172d47beb14d7d905b20077ab4e678d9d6
AriT93 <arit93@gmail.com>
2011-07-09 15:21:45 -0500

    update adding calls to the list
    update script.js so that the calls are added to the list correctly and that the div for the overlay is created.  Will need to add some more to make it a little cleaner but hey it works now so theres that....

--- public/js/script.js
+++ public/js/script.js
@@ -4,13 +4,64 @@
 */
 
 
+function buildCall(obj,callList){
+    var li = $('<li>');
+    var li2 = $('<li>');
+    var bad = ['_id', 'latitude','longitude','age', 'state'];
+    for (var key in obj){
+        if(obj.hasOwnProperty(key)){
+            if($.inArray(key, bad) == -1){
+                var d = $('<div>');
+                var d2 = $('<div>');
+                d.addClass("grid_2");
+                d2.addClass("grid_2");
+                if(key == 'name'){
+                    d.addClass('alpha');
+                    d2.addClass('alpha');
+                }
+                d.html(obj[key]);
+                d2.html(obj[key]);
+                li.append(d);
+                li2.append(d2);
+
+            }
+        }
+    }
+    if(callList){
+        var d = $('<div>');
+        d.addClass("grid_2");
+        d.addClass("omega");
+        var b = $("<button rel='#"+ obj.tn +"_ov'>");
+        var img=$("<img class='phone_icon' src='/img/phone.png'/>");
+        d.append(b);
+        b.append(img);
+        li.addClass("call");
+        li.attr('id',obj.tn);
+        li.append(d);
+    }
+    $("#callList li:first").after(li);
+    //add to the overlay
+     var ovdiv = $('<div id="' + obj.tn + '_ov">');
+     ovdiv.addClass("simple_overlay");
+     var img2 =$("<img src='/img/phone.png'/>");
+     var grid = $('<div>');
+     grid.addClass('grid_12');
+     grid.attr('id','calls');
+     var ulcall = $('<ul>');
+     ulcall.append(li2);
+     grid.append(ulcall);
+     ovdiv.append('<a class="close"></a>');
+     ovdiv.append(img2);
+     ovdiv.append(grid);
+    $("#calls_ov").append(ovdiv);
+    $("button[rel]").overlay();
+
+}
 
 function message(obj){
     var list="";
 
     if('message' in obj){
-//        el.innerHTML = "<b>" + obj.message[0] + "</b>";
-
     }else if('announcement' in obj){
         $('<p>').html(obj.announcement).appendTo($("#messages"));
     }
@@ -18,11 +69,13 @@ function message(obj){
         for (var i in obj.result){
             Call = JSON.parse(obj.result[i]);
             if(obj.result[i] != undefined ){
-                list += "<li>" + Call.name + " " + Call.tn + "</li>";
+                // list += "<li>" + Call.name + " " + Call.tn + "</li>";
+                buildCall(Call,true);
             }
         }
         if(list != ""){
-          $('<p>').html(list).prependTo($("#chat"));
+            $('<p>').html(list).appendTo($("#calls"));
+
         }
     }
 };
@@ -53,13 +106,13 @@ function updateLocation() {
       var state = "";
       $.each(loc.address_components,function(i,v) {
          if ($.inArray("locality",v.types) > -1) {
-           city = v.short_name; 
+           city = v.short_name;
          }
          if ($.inArray("administrative_area_level_1",v.types) > -1) {
-           state = v.short_name; 
+           state = v.short_name;
          }
       });
-      var lat = loc.geometry.location.lat(); 
+      var lat = loc.geometry.location.lat();
       var lng = loc.geometry.location.lng();
       $("#latitude").val(lat);
       $("#longitude").val(lng);
@@ -101,9 +154,10 @@ $(document).ready(function(){
         Call.latitude  = $("#latitude").val();
         Call.longitude = $("#longitude").val();
         socket.send(JSON.stringify(Call));
-        $("#name").val("");
-        $("#tn").val("");
         }
+        $(':input',"#call")
+        .not(':button, :reset, :submit, :hidden')
+        .val('');
         return false;
     });
 
